bbb21440fa032442771514171b5447371a26dc89
bhadresh-lamprostech <bhadresh.lamprostech@gmail.com>
2024-05-07 14:59:57 +0530

    home page integraion with tab

--- components/home/Collections.js
+++ components/home/Collections.js
@@ -1,15 +1,33 @@
-"use client";
 import { useEffect, useState } from "react";
-import { collections } from "@/data/collections";
 import tippy from "tippy.js";
 import Image from "next/image";
+import { ClipLoader } from "react-spinners";
 import Link from "next/link";
+
 const labels = ["Last 24 Hours", "Last 7 Days", "Last 30 Days"];
+
 export default function Collections() {
   const [activeLabel, setActiveLabel] = useState(labels[0]);
+  const [collections, setCollections] = useState([]);
+  const currentUrl = process.env.NEXT_PUBLIC_URL;
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${currentUrl}/api/get-collection-home`);
+        const data = await response.json();
+        console.log(data);
+        setCollections(data);
+      } catch (error) {
+        console.error("Error fetching collections:", error);
+      }
+    };
+
+    fetchData();
     tippy("[data-tippy-content]");
-  }, []);
+  }, [collections.length < 0]);
+
+  console.log(collections);
+
   return (
     <section className="relative py-24 dark:bg-jacarta-800">
       <picture className="pointer-events-none absolute inset-0 -z-10 dark:hidden">
@@ -62,57 +80,67 @@ export default function Collections() {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 gap-3 md:grid-cols-2 md:gap-[1.875rem] lg:grid-cols-4">
-          {collections.map((elm, i) => (
-            <div
-              key={i}
-              className="flex rounded-2.5xl border border-jacarta-100 bg-white py-4 px-7 transition-shadow hover:shadow-lg dark:border-transparent dark:bg-jacarta-700"
-            >
-              <figure className="mr-4 rtl:mr-0 rtl:ml-4 shrink-0">
-                <Link href={`/collection/${elm.id}`} className="relative block">
-                  <Image
-                    width={48}
-                    height={48}
-                    src={elm.avatar}
-                    alt="avatar 1"
-                    className="rounded-2lg"
-                    loading="lazy"
-                  />
-                  <div className="absolute -left-3 top-1/2 flex h-6 w-6 -translate-y-2/4 items-center justify-center rounded-full border-2 border-white bg-jacarta-700 text-xs text-white dark:border-jacarta-600">
-                    {elm.id}
-                  </div>
-                  {elm.verified && (
-                    <div
-                      className="absolute -left-3 top-[60%] flex h-6 w-6 items-center justify-center rounded-full border-2 border-white bg-green dark:border-jacarta-600"
-                      data-tippy-content="Verified Collection"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        width="24"
-                        height="24"
-                        className="h-[.875rem] w-[.875rem] fill-white"
+        {collections.length > 0 ? (
+          <div className="grid grid-cols-1 gap-3 md:grid-cols-2 md:gap-[1.875rem] lg:grid-cols-4">
+            {collections.map((elm, i) => (
+              <div
+                key={i}
+                className="flex rounded-2.5xl border border-jacarta-100 bg-white py-4 px-7 transition-shadow hover:shadow-lg dark:border-transparent dark:bg-jacarta-700"
+              >
+                <figure className="mr-4 rtl:mr-0 rtl:ml-4 shrink-0">
+                  <Link href={`/collection/${elm.address}`} passHref>
+                    <div className="relative block">
+                      <Image
+                        width={48}
+                        height={48}
+                        src={elm.contractUri}
+                        alt="avatar"
+                        className="rounded-2lg"
+                        loading="lazy"
+                      />
+                      {/* <div className="absolute -left-3 top-1/2 flex h-6 w-6 -translate-y-2/4 items-center justify-center rounded-full border-2 border-white bg-jacarta-700 text-xs text-white dark:border-jacarta-600">
+                        {elm.address}
+                      </div> */}
+
+                      <div
+                        className="absolute -left-3 top-[60%] flex h-6 w-6 items-center justify-center rounded-full border-2 border-white bg-green dark:border-jacarta-600"
+                        data-tippy-content="Verified Collection"
                       >
-                        <path fill="none" d="M0 0h24v24H0z" />
-                        <path d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z" />
-                      </svg>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          width="24"
+                          height="24"
+                          className="h-[.875rem] w-[.875rem] fill-white"
+                        >
+                          <path fill="none" d="M0 0h24v24H0z" />
+                          <path d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z" />
+                        </svg>
+                      </div>
                     </div>
-                  )}
-                </Link>
-              </figure>
-              <div className="">
-                <Link href={`/collection/${elm.id}`} className="block">
-                  <span className="font-display font-semibold text-jacarta-700 hover:text-accent dark:text-white">
-                    {elm.name}
+                  </Link>
+                </figure>
+                <div className="">
+                  <Link href={`/collection/${elm.address}`} passHref>
+                    <div className="block">
+                      <span className="font-display font-semibold text-jacarta-700 hover:text-accent dark:text-white">
+                        {elm.name}
+                      </span>
+                    </div>
+                  </Link>
+                  <span className="text-sm dark:text-jacarta-300">
+                    {elm.price}
                   </span>
-                </Link>
-                <span className="text-sm dark:text-jacarta-300">
-                  {elm.price}
-                </span>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        ) : (
+          <div className="text-center">
+            <ClipLoader />
+          </div>
+        )}
+
         <div className="mt-10 text-center">
           <Link
             href="/rankings"
